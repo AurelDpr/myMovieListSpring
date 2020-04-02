56f679a448912f4c655cf9ed1ae69cc7dead0c93
Aurelien Duperray <duperray.2002@apside-groupe.com>
2020-04-02 09:48:44 +0200

    deployement

--- src/main/java/com/example/myMovieListSpring/MyMovieListSpringApplication.java
+++ src/main/java/com/example/myMovieListSpring/MyMovieListSpringApplication.java
@@ -2,9 +2,17 @@ package com.example.myMovieListSpring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.WebApplicationInitializer;
 
 @SpringBootApplication
-public class MyMovieListSpringApplication {
+public class MyMovieListSpringApplication extends SpringBootServletInitializer implements WebApplicationInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MyMovieListSpringApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyMovieListSpringApplication.class, args);
